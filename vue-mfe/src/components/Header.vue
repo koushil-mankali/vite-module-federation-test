d07ecc7e54db22683f32b758e6778ddb43c35e04
koushil-mankali <koushilmankali@gmail.com>
2022-10-20 11:10:43 +0530

    Error occured

---
+++ vue-mfe/src/components/Header.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts"></script>
+
+<template>
+    <nav class="nav">
+        <div class="ttl">Micro Frontend Test App</div>
+        <div class="menu">
+            <div class="menuItem">Home</div>
+            <div class="menuItem">About</div>
+            <div class="menuItem">Contact</div>
+            <div class="menuItem">Vue app</div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.nav{
+    width: 100%;
+    height: 70px;
+    background-color: #ffb703;  
+    box-shadow: 2px 2px 2px #e9c46a;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.ttl{
+    font-weight: 600;
+    font-size: 2.5rem;
+    color: #f1faee;
+    cursor: pointer;
+}
+
+.menu{
+    display: flex;
+    font-size: 1.4rem;
+    font-weight: 500;
+    color: #f7ede2;
+}
+
+.menuItem{
+    margin: 0.5rem;
+    cursor: pointer;
+}
+</style>
--- vue-mfe/vite.config.js
+++ vue-mfe/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import federation from "@originjs/vite-plugin-federation";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    federation({
+      name: "vueheader",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Header": "./src/components/Header.vue",
+      },
+      shared: ["vue"],
+    }),
+  ],
+  build:{
+    target: "esnext"
+  }
+});
